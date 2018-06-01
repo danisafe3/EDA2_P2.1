567632503592e6394d125df934f0ba9605a574f4
JoanMoya99 <38790800+JoanMoya99@users.noreply.github.com>
2018-06-01 08:11:11 +0000

    Create file_utils.c

---
+++ file_utils.c
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   tree.h
+ * Author: abravo
+ *
+ * Created on May 27, 2018, 10:48 AM
+ */
+
+#ifndef TREE_H
+#define TREE_H
+
+#include "word_info.h"
+
+
+typedef struct {
+    
+} Tree;
+
+void init_tree(Tree* t);
+void clear_tree(Tree* t);
+int size_tree(Tree* t);
+void insert_into_tree(Tree* t, WordInfo wi);
+WordInfo* find_in_tree(Tree* t, char* word);
+void print_tree(Tree* t);
+
+#endif /* TREE_H */
+
