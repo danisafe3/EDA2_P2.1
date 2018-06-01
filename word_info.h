c252f5496f613f603f95b53f1f45d9c6115df501
danisafe3 <37106410+danisafe3@users.noreply.github.com>
2018-06-01 09:39:28 +0200

    Add files via upload

---
+++ word_info.h
@@ -0,0 +1,25 @@
+#ifndef WORDINFO_H
+#define WORDINFO_H
+
+#define MAX_WORD_LENGTH 30
+#define MAX_DEFINITION_LENGTH 100
+
+typedef struct {
+    char word[MAX_WORD_LENGTH];
+    char definition[MAX_DEFINITION_LENGTH];
+    char pos;                              
+} WordInfo;
+
+struct nodes{
+    WordInfo wi;
+    struct nodes *father;
+    struct nodes *left_tree;
+    struct nodes *right_tree;
+};
+typedef struct nodes Node;
+
+void create_word_info(WordInfo* wi, char* word, char* definition, char pos);
+char* get_word(WordInfo* wi);
+void print_word_info(WordInfo wi);
+
+#endif
