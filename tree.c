4fa3939e82bca953e9e483a0f6a21cb17b157d3c
JoanMoya99 <38790800+JoanMoya99@users.noreply.github.com>
2018-06-01 08:10:11 +0000

    Create tree.c

---
+++ tree.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "commons.h"
+#include "tree.h"
+
+
+
+// NODE
+/*
+void clearPostOrder(Node* node)
+{ 
+    
+}
+
+Node *createNode(WordInfo wi)
+{
+    return NULL;
+}
+
+Node* insertNode(Node* node, WordInfo wi){
+    
+    return NULL;
+}
+
+Node* findNode(Node* node, char* word)
+{
+    return NULL;
+}
+
+void printInOrder(Node* node)
+{ 
+
+}*/
+
+// TREE
+
+void init_tree(Tree* t){
+   
+}
+
+void clear_tree(Tree* t){
+    
+}
+
+void insert_into_tree(Tree* t, WordInfo wi){
+    
+}
+
+WordInfo* find_in_tree(Tree* t, char* word){
+    
+    return NULL;
+}
+
+int size_tree(Tree* t){
+    return 0;
+}
+
+void print_tree(Tree* t){
+    
+}
+
