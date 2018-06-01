4960d26bb256b40b1ca561740eac91a145bb2acd
JoanMoya99 <38790800+JoanMoya99@users.noreply.github.com>
2018-06-01 08:09:26 +0000

    Create main.h

---
+++ main.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.h
+ * Author: francesco
+ *
+ * Created on May 4, 2018, 7:37 PM
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#define INFO_FILE_PATH "data_dict.tsv"
+#define QUERY_FILE_PATH "data_list.tsv"
+
+
+#endif /* MAIN_H */
+
