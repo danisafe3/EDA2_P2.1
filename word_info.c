87d02de7af013fc9473f344f5a715dbacc5d5aa5
Iván Martínez Fernández <37107183+ivanmafe@users.noreply.github.com>
2018-06-07 17:24:46 +0200

    Update word_info.c

--- word_info.c
+++ word_info.c
@@ -4,17 +4,27 @@
 #include "file_utils.h"
 
 void print_word_info(WordInfo wi) {
-    printf("-> %s (%c): %s\n", "WORD", 'P', "DEFINITION");
+    printf("-> %s (%c): %s\n", wi.word, wi.pos, wi.definition);
 }
 
 // Returns the word to be used for hashing
 char* get_word(WordInfo* wi) {
-    return wi->word;
+    
+    if (wi->word == "\0"){
+        printf("ERROR : This word does not exist! \n");
+        return NULL;
+    }
+    else 
+        return (wi->word);
+
 }
 
 // Returns the word to be used for hashing
 void create_word_info(WordInfo* wi, char* word, char* definition, char pos) {
-    strncpy(wi->word, word, MAX_WORD_LENGTH);
-    strncpy(wi->definition, definition, MAX_DEFINITION_LENGTH);
+    
+    strcpy(wi->word , word);
+    strcpy(wi->definition , definition); 
     wi->pos = pos;
+    
+
 }
